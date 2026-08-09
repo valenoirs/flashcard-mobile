1758841a83a48fcc0c03d4b995dff4a4cc1e1988
valenoirs <luckyvw.work@gmail.com>
2026-08-10 02:07:04 +0700

    refactor: run formatter in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,25 +1,25 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { SafeAreaProvider } from "react-native-safe-area-context"
 
-import DeckScreen from "./screen/DeckScreen";
+import DeckScreen from "./screen/DeckScreen"
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-    return (
-        <SafeAreaProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Deck">
-                    <Stack.Screen
-                        name="Deck"
-                        component={DeckScreen}
-                        options={{ headerShown: false }}
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Deck">
+          <Stack.Screen
+            name="Deck"
+            component={DeckScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
 }
 
 // const styles = StyleSheet.create({
